67d33f62048fdeb09e7506293ab06f48ebd519fb
Jean le Grand Bokassa <jean.le.grand.bokassa@gmail.com>
2024-04-18 11:30:01 +0200

    updated application and readme" "

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# My Awesome Blog
-This is my awesome blog. It's about everything I love. I hope you enjoy it!
+# My awesome Symfony 7 project
+
+This is a symfony project for testing purposes and linters.
+
+## Requirements
+
+- PHP 8.1 or higher
+- Composer
+- Symfony CLI (optional, for convenience)
+
+## Installation
+
+### Clone the Repository
+
+Start by cloning the repository to your local machine:
+
+```bash
+git clone https://github.com/your-username/your-project-name.git
+cd your-project-name
+
+### Install Composer Dependencies
+
+```bash
+composer install
+```
+
+### Create a .env File
+
+```bash
+cp .env .env.local
+```
+
+### Create the database and execute migrations
+
+```bash
+php bin/console doctrine:database:create
+php bin/console doctrine:migrations:migrate
+```
+
---
+++ src/Controller/CommentController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Comment;
+use App\Form\CommentType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CommentController extends AbstractController
+{
+    #[Route('/comment', name: 'app_comment')]
+    public function index(): Response
+    {
+        return $this->render('comment/index.html.twig', [
+            'controller_name' => 'CommentController',
+        ]);
+    }
+
+    #[Route('/comment/new/{id}', name: 'app_comment_new', methods: ['GET','POST'])]
+    public function new(Request $request, Article $article, EntityManagerInterface $entityManager): Response
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setArticle($article);  // Set the article to which the comment belongs
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_article_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('comment/new.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+}
